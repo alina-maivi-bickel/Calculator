f02f3e968a5db2ee3ea41bf0d088feadde243821
Alina Bickel <alinamaivi.bickel@gmail.com>
2025-08-03 23:09:01 +0200

    Implemented a layered architecture

--- src/main/java/com/alina/calculator/Main.java
+++ src/main/java/com/alina/calculator/Main.java
@@ -1,30 +1,17 @@
 package com.alina.calculator;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     public static void main(String[] args) {
-        launch(args);
-        
+        launch(args); 
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
-        stage.setTitle("Calculator");
-        Image icon = new Image(getClass().getResourceAsStream("/calculator.png"));
-        stage.getIcons().add(icon);
-
-        stage.setScene(scene); 
-
-        stage.show();
+        GUI gui = new GUI();
+        gui.start(stage);
     }
 }
