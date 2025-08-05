7028b9273112a034b146fb42c09f76032ea83a12
Alina Bickel <alinamaivi.bickel@gmail.com>
2025-08-05 03:52:14 +0200

    implemented all the buttons and a textfield to the gui

--- src/main/java/com/alina/calculator/GUI.java
+++ src/main/java/com/alina/calculator/GUI.java
@@ -1,41 +1,73 @@
 package com.alina.calculator;
 
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class GUI {
 
   public void start(Stage stage) throws Exception {
     
-    Group root = new Group();
-    Scene scene = new Scene(root, Color.LIGHTSKYBLUE);
-    Image icon = new Image(getClass().getResourceAsStream("/calculator.png"));
+    VBox root = new VBox(5);
+    root.setPadding(new Insets(20));
+    root.setAlignment(Pos.TOP_CENTER);
+    root.getStyleClass().add("vbox");
 
-    Image addButton = new Image(getClass().getResourceAsStream("/buttons/plus.png"));
-    Image subButton = new Image(getClass().getResourceAsStream("/buttons/minus.png"));
-    Image mulButton = new Image(getClass().getResourceAsStream("/buttons/sternchen.png"));
-    Image divButton = new Image(getClass().getResourceAsStream("/buttons/schragstrich.png"));
+    //Field at the top
+    TextField field = new TextField();
+    field.setEditable(false);
+    field.setText("0");
+    field.setFont(Font.font("Monospaced", 50));
+    field.setMaxWidth(350);
+    field.setAlignment(Pos.CENTER_RIGHT);
+    field.setFocusTraversable(false);
+    VBox.setMargin(field, new Insets(5, 0, 5, 0));
 
-    ImageView add = new ImageView(addButton);
-    ImageView sub = new ImageView(subButton);
-    ImageView mul = new ImageView(mulButton);
-    ImageView div = new ImageView(divButton);
-    ImageView[] buttons = {add, sub, mul, div};
-    int[] yPositions = {80, 180, 280, 380};
+    //Arithmetic and Number Buttons
+    GridPane grid = new GridPane();
+    grid.setHgap(10.0);
+    grid.setVgap(10.0);
+    grid.setPadding(new Insets(5, 10, 0, 10));
 
-    for(int i = 0; i < buttons.length; i++) {
-      buttons[i].setX(300);
-      buttons[i].setY(yPositions[i]);
-      buttons[i].setFitWidth(50);
-      buttons[i].setPreserveRatio(true);
+    String[] numberNames = {"seven", "eight", "nine", "plus", 
+                            "four", "five", "six", "minus", 
+                            "one", "two", "three" , "sternchen", 
+                            "delete", "zero", "equals", "schragstrich"};
+
+    for (int i = 0; i < numberNames.length; i++) {
+      String path = "/buttons/" + numberNames[i] + ".png";
+      ImageView view = new ImageView(new Image(getClass().getResourceAsStream(path)));
+      view.setFitWidth(60);
+      view.setPreserveRatio(true);
+
+      Button btn = new Button();
+      btn.setGraphic(view);
+      btn.setStyle("-fx-background-color: transparent; -fx-border-color: transparent;");
+
+      int row = i / 4;
+      int col = i % 4;
+
+      grid.add(btn, col, row);
     }
 
-    root.getChildren().addAll(add, sub, mul, div);
+    root.getChildren().addAll(field, grid);
 
+    Scene scene = new Scene(root, 400, 500, Color.LIGHTSKYBLUE);
+    Image icon = new Image(getClass().getResourceAsStream("/calculator.png"));
+
+    //CSS-Styling
+    scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+    
+    //Stage
     stage.setTitle("Calculator");
     stage.getIcons().addAll(icon);
     stage.setWidth(400);
